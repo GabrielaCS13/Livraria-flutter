c5ff5b26697a28647ed581a5d6d75a7cb73dd01f
GabrielaCS13 <gabriela.silva.00243@gmail.com>
2023-09-05 17:44:24 -0300

    adicinando o projeto em flutter

---
+++ lib/data/dblivro.dart
@@ -0,0 +1,91 @@
+import 'package:path/path.dart'; 
+
+import 'package:sqflite/sqflite.dart'; 
+
+  
+
+class MyDb { 
+
+  late Database db; 
+
+  
+
+  Future open() async { 
+
+    // Get a location using getDatabasesPath 
+
+    var databasesPath = await getDatabasesPath(); 
+
+    String path = join(databasesPath, 'bdcrud.db'); 
+
+    //join is from path package 
+
+    print( 
+
+        path); //output /data/user/0/com.testapp.flutter.testapp/databases/demo.db 
+
+  
+
+    db = await openDatabase(path, version: 1, 
+
+        onCreate: (Database db, int version) async { 
+
+      // When creating the db, create the table 
+
+      await db.execute(''' 
+
+                  CREATE TABLE IF NOT EXISTS livro (  
+
+                        id primary key, 
+
+                        nome varchar(255) not null, 
+
+                        descricao varchar(255) not null, 
+
+                        autor varchar(255) not null,
+
+                        preco varchar( 8) not null, 
+
+                        roll_no int not null
+
+                    ); 
+
+  
+
+                    //create more table here 
+
+                 
+
+                '''); 
+
+      print("Tabela Criada com Sucesso!"); 
+
+    }); 
+
+  } 
+
+  
+
+  //método de consulta de dados 
+
+  Future<Map<dynamic, dynamic>?> getLivro(int rollno) async { 
+
+    List<Map> maps = 
+
+        await db.query('livro', where: 'roll_no = ?', whereArgs: [rollno]); 
+
+    //getting student data with roll no. 
+
+    if (maps.length > 0) { 
+
+      return maps.first; 
+
+    } 
+
+    return null; 
+
+  } 
+
+} 
+
+ 
